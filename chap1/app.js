315a3422eeac50b1cd0dc5c7d4da4eb5ba83382a
Your Name <you@example.com>
2025-08-01 18:16:38 +0500

    assignment

---
+++ chap1/app.js
@@ -0,0 +1,27 @@
+  // QUESTION 1 
+
+  alert("👋 Hello there! 😊 Hope you're having a nice day!");
+
+
+
+   // QUESTION 2
+
+   alert("Error! Please enter a valid password.")
+
+
+   // QUESTION 3
+
+   alert("Welcom to JS Land...\n HAPPY CODING!")
+
+
+   // QUESTION 4 
+
+   alert("Welcome to JS Land")
+
+
+   alert("HAPPY CODING! \n Prevent this page from creating additional dialogs.")
+
+
+   // QUESTION 5 
+   
+   alert("Hello.. I can run JS through my web's browser console.")
