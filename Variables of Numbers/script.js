2b9894e7d3e3441cb091f137b16dd7370502c4fb
Your Name <you@example.com>
2025-08-01 23:29:12 +0500

    number of variables

---
+++ Variables of Numbers/script.js
@@ -0,0 +1,24 @@
+// Quetion 1
+
+var age = "i'm 15 Years old";
+alert(age)
+
+// Quetion 2
+
+var visit = 15;
+alert(" You have visited this website " + visit + " times" )
+ 
+
+// Quetion 3
+
+var birthyear = "My Birth Year is 2003";
+document.write( "My Birth Year is 2003 <br>")
+document.write ("  Data type of my declered number of string ")
+
+
+// Question 4 
+
+
+var visitor = " john";
+var products = " 5 t-shirts";
+document.write("<br>" + visitor  + " ordered " + products + " On XYZ Clothing Store") 
