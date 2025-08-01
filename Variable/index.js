8f80189851a00d0b374e4efcfd0554fe9d121cb1
Your Name <you@example.com>
2025-08-01 19:30:12 +0500

    variable

---
+++ Variable/index.js
@@ -0,0 +1,73 @@
+// Question 1 
+
+var username = "";
+
+// Question 2
+
+var myname = " Kashaf Shaikh ";
+
+
+// Question 3
+
+var tittleMsg;
+var msg = "Hello World";
+alert (msg)
+
+
+// Question 4 
+
+
+// Q:4 a 
+
+var name ="jhone dhoe";
+alert(name)
+
+// Q:4 b
+
+var age ="15 years old";
+alert(age)
+
+
+// Q:4 c
+
+var text ="Certified Mobile Application Development";
+alert(text)
+
+
+// Qustion 5
+
+var pizza = "food";
+alert("pizza \n pizz \n piz \n pi \n p")
+
+
+// Qustion 6
+
+var para = " My Email Address is "
+var email = " kashaf@gmail.com "
+alert( para + email)
+
+
+// Qustion 7
+
+var line = " I am trying to learn from the book "
+var book = " A Smarter Way To learn Javascript "
+alert( line + book)
+
+
+// Qustion 8 
+
+document.write("Yah! I can write HTML content through Javascript!")
+
+
+// Qustion 9
+
+var variable = "*_____________ ====== Hello User ====== __________ *";
+alert(variable)
+
+
+
+
+
+
+
+
