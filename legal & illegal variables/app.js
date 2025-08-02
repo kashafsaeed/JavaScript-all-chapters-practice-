ceb5c7e6ec498d14006821c1dae1b702c0048e11
Your Name <you@example.com>
2025-08-02 13:31:37 +0500

    legal $ illegal variable

---
+++ legal & illegal variables/app.js
@@ -0,0 +1,54 @@
+// Question 1 
+var 
+a = 1,
+b = 2,
+c = 3;
+
+// Question 2 
+
+
+
+
+// Legal Variable Names 
+
+// 1 name --> Starts with a letter – valid
+// 2 firstname --> Uses camelCase – valid
+// 3 _user --> 	Starts with underscore – valid
+// 4 $amount --> Starts with dollar sign – valid
+// 5 user1 --> Starts with letter, contains number – valid
+
+
+
+// Illegal Variable Names 
+
+// 1 :  1name --> 	Starts with a number – invalid
+// 2 :  my-name -->  Hyphen - is not allowed – invalid
+// 3 : !value -->  Special character ! is not allowed – invalid
+// 4 : user name --> Spaces are not allowed in variable names – invalid
+// 5 : function -->  	Reserved keyword – invalid
+
+
+
+
+
+// Question 3 
+
+// part (a)
+
+document.write("<h1> Rules for naming JS Variables </h1>");
+
+// part (b)
+
+document.write("<p> Variable names can only contain , number, $ and _ . For example : $my_1stVariable </p>");
+
+// part (c)
+
+document.write("<p> Variable must begin with a letter, $ or _ . For example : $name,_name or name </p> ");
+
+// part (d)
+
+document.write("<p> Variable names are case sensitive </p> ");
+
+// part (e)
+
+document.write("<p> Variable names should not be je keywords </p> ");
